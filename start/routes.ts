dc26806cfef11d6e56b0c3d69bf0adb3386b3e4f
ebata-paly55 <rafael.ebata@play55.com.br>
2025-08-12 17:03:22 -0400

    "atualizando autenticação"

--- start/routes.ts
+++ start/routes.ts
@@ -11,12 +11,12 @@ import UsersController from '#controllers/users_controller'
 import router from '@adonisjs/core/services/router'
 import SessionController from '#controllers/session_controller'
 import TodosController from '#controllers/todos_controller'
-// import { middleware } from './kernel.js'
+import { middleware } from './kernel.js'
 
 router.post('/login', [SessionController, 'store'])
 router.delete('/logout', [SessionController, 'destroy'])
 
-// router.group(() => {
+router.group(() => {
     
     router.get('/users', [UsersController, 'index'])
     router.get('/user/:id', [UsersController, 'show'])
@@ -30,6 +30,6 @@ router.delete('/logout', [SessionController, 'destroy'])
     router.put('/todo/:id', [TodosController, 'update'])
     router.delete('/todo/:id', [TodosController, 'destroy'])
 
-// }).use(middleware.auth({guards: ['api']}))
+}).use(middleware.auth({guards: ['api']}))
 
 
